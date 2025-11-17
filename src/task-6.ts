9a543f30c21d9d7fe979584d5b8148249b60a02a
Ivan227-hub <i.kushkov-de21@lll.kpi.ua>
2025-11-17 14:46:49 +0200

    Initial commit - tasks 1 to 8

---
+++ src/task-6.ts
@@ -0,0 +1,10 @@
+// task-6.ts
+function getFirstElement<T>(arr: T[]): T {
+  return arr[0];
+}
+
+const firstNumber = getFirstElement<number>([1, 2, 3]); // 1
+const firstString = getFirstElement<string>(["a", "b", "c"]); // "a"
+const firstBoolean = getFirstElement<boolean>([true, false, true]); // true
+
+console.log(firstNumber, firstString, firstBoolean);
